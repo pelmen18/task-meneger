8aee6e0775820646ab4b83757ab2db74199773ba
zmiyka <povazhnyirostyslav@gmail.com>
2025-05-17 13:42:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ def search_task(task):
     else:
         print(f"{task} не знайдено.")
 
+def chose_difficulty(level):
+
+    dificult_tasks = [task for task in TaskList if task["складність"] == level.lower()]
+
+    if dificult_tasks:
+        print(f"\nСправи зі складністю '{level}':")
+        for i, task in enumerate(dificult_tasks, 1):
+            print(f"{i}. {task['назва']}")
+    else:
+        print(f"\nНемає справ зі складністю '{level}'.")
+
+_input = input("Оберіть складність (легка / середня / складна): ").strip().lower()
+chose_difficulty(_input)
