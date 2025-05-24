0ccf90ff7dd517fe80b74157b70a7e9388c060d9
zmiyka <povazhnyirostyslav@gmail.com>
2025-05-24 14:20:55 +0100

    functions

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-def search_task(task):
-    results = [task for task in TaskList if task.lower()]
-    if results:
-        print(f"{task}")
-    else:
-        print(f"{task} не знайдено.")
+# def search_task(task):
+#     results = [task for task in TaskList if task.lower()]
+#     if results:
+#         print(f"{task}")
+#     else:
+#         print(f"{task} не знайдено.")
+
 
 def chose_difficulty(level):
-
     dificult_tasks = [task for task in TaskList if task["складність"] == level.lower()]
 
     if dificult_tasks:
@@ -20,13 +20,15 @@ _input = input("Оберіть складність (легка / середня
 chose_difficulty(_input)
 
 
-def add_task():
-    name = input("Введіть назву справи: ").strip()
-    difficulty = input("Введіть складність (легка / середня / складна): ").strip().lower()
+# def add_task():
+#     name = input("Введіть назву справи: ").strip()
+#     difficulty = input("Введіть складність (легка / середня / складна): ").strip().lower()
     
-    if difficulty not in ["легка", "середня", "складна"]:
-        print("Такої складності немає")
-        return
+#     if difficulty not in ["легка", "середня", "складна"]:
+#         print("Такої складності немає")
+#         return
 
-    task = {"назва": name, "складність": difficulty}
-    TaskList.append(task)
+#     task = {"назва": name, "складність": difficulty}
+#     TaskList.append(task)
+
+
